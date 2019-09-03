a96e5bf3d8932ca63d69a5f520e1f44c6d2eb979
tanjinjie <a@PeterdeMacBook-Pro.local>
2019-09-03 12:29:41 +0800

    完成配置选项

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const parser = require('@babel/parser');
 const traverse = require('@babel/traverse').default; // traverse 采用的 ES Module 导出，我们通过 requier 引入的话就加个 .default
 const babel = require('@babel/core');
-
+const { entry: entrys } = require('./minit-webpack.config');
 const read = filename => {
   const buffer = fs.readFileSync(filename, 'utf-8');
   const AST = parser.parse(buffer, { sourceType: 'module' });
@@ -33,38 +33,38 @@ const read = filename => {
   };
 };
 
-
 // 创建依赖图谱函数, 递归遍历所有依赖模块
-const makeDependenciesGraph = (entry) => {
-	const entryModule = read(entry)
-	const graghArray = [ entryModule ]; // 首先将我们分析的入口文件结果放入图谱数组中
-	for (let i = 0; i < graghArray.length; i ++) {
+const makeDependenciesGraph = entry => {
+  const entryModule = read(entry);
+  const graghArray = [entryModule]; // 首先将我们分析的入口文件结果放入图谱数组中
+  for (let i = 0; i < graghArray.length; i++) {
     const item = graghArray[i];
-		const { dependencies } = item; // 拿到当前模块所依赖的模块
-		if (dependencies) {
-			for ( let j in dependencies ) { // 通过 for-in 遍历对象
-				graghArray.push(read(dependencies[j])); // 如果子模块又依赖其它模块，就分析子模块的内容
-			}
-		}
-	}
+    const { dependencies } = item; // 拿到当前模块所依赖的模块
+    if (dependencies) {
+      for (let j in dependencies) {
+        // 通过 for-in 遍历对象
+        graghArray.push(read(dependencies[j])); // 如果子模块又依赖其它模块，就分析子模块的内容
+      }
+    }
+  }
 
-	const gragh = {}; // 将图谱的数组形式转换成对象形式
-	graghArray.forEach( item => {
-		gragh[item.filename] = {
-			dependencies: item.dependencies,
-			code: item.code
-		}
-  })
-  console.log(gragh)
-	return gragh;
-}
+  const gragh = {}; // 将图谱的数组形式转换成对象形式
+  graghArray.forEach(item => {
+    gragh[item.filename] = {
+      dependencies: item.dependencies,
+      code: item.code
+    };
+  });
+  console.log(gragh);
+  return gragh;
+};
 
-const generateCode = (entry) => {
-	// 注意：我们的 gragh 是一个对象，key是我们所有模块的绝对路径，需要通过 JSON.stringify 来转换
-	const gragh = JSON.stringify(makeDependenciesGraph(entry));
-	// 我们知道，webpack 是将我们的所有模块放在闭包里面执行的，所以我们写一个自执行的函数
-	// 注意: 我们生成的代码里面，都是使用的 require 和 exports 来引入导出模块的，而我们的浏览器是不认识的，所以需要构建这样的函数
-	return `
+const generateCode = entry => {
+  // 注意：我们的 gragh 是一个对象，key是我们所有模块的绝对路径，需要通过 JSON.stringify 来转换
+  const gragh = JSON.stringify(makeDependenciesGraph(entry));
+  // 我们知道，webpack 是将我们的所有模块放在闭包里面执行的，所以我们写一个自执行的函数
+  // 注意: 我们生成的代码里面，都是使用的 require 和 exports 来引入导出模块的，而我们的浏览器是不认识的，所以需要构建这样的函数
+  return `
 		(function( gragh ) {
 			function require( module ) {
 				// 相对路径转换成绝对路径的方法
@@ -78,14 +78,11 @@ const generateCode = (entry) => {
 
 				return exports;
 			}
-			require('${ entry }')
-		})(${ gragh })
+			require('${entry}')
+		})(${gragh})
 	`;
-}
+};
 
-const code = generateCode('./app.js');
+const code = generateCode(entrys);
 
-console.log(code)
-
-
-
+console.log(code);
